92aeb5c4578a6fd22ccd98ac9f807e9ab2a897f6
DESKTOP-842M40V\kskim <kskim@castis.com>
2025-01-13 10:55:16 +0900

    first commit

---
+++ src/auth/view/jwt/jwt-sign-in-view.tsx
@@ -0,0 +1,159 @@
+'use client';
+
+import { z as zod } from 'zod';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Alert from '@mui/material/Alert';
+import IconButton from '@mui/material/IconButton';
+import LoadingButton from '@mui/lab/LoadingButton';
+import InputAdornment from '@mui/material/InputAdornment';
+
+import { paths } from 'src/routes/paths';
+import { useRouter } from 'src/routes/hooks';
+import { RouterLink } from 'src/routes/components';
+
+import { useBoolean } from 'src/hooks/use-boolean';
+
+import { Iconify } from 'src/components/iconify';
+import { Form, Field } from 'src/components/hook-form';
+
+import { useAuthContext } from '../../hooks';
+import { FormHead } from '../../components/form-head';
+import { signInWithPassword } from '../../context/jwt';
+
+// ----------------------------------------------------------------------
+
+export type SignInSchemaType = zod.infer<typeof SignInSchema>;
+
+export const SignInSchema = zod.object({
+  userId: zod
+    .string()
+    .min(1, { message: 'userId is required!' }),
+  password: zod
+    .string()
+    .min(1, { message: 'Password is required!' })
+    .min(4, { message: 'Password must be at least 4 characters!' }),
+});
+
+// ----------------------------------------------------------------------
+
+export function JwtSignInView() {
+  const router = useRouter();
+
+  const { checkUserSession } = useAuthContext();
+
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const password = useBoolean();
+
+  const defaultValues = {
+    userId: 'test',
+    password: '1234',
+  };
+
+  const methods = useForm<SignInSchemaType>({
+    resolver: zodResolver(SignInSchema),
+    defaultValues,
+  });
+
+  const {
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      await signInWithPassword({ userId: data.userId, password: data.password });
+      await checkUserSession?.();
+
+      router.refresh();
+    } catch (error) {
+      console.error(error);
+      setErrorMsg(typeof error === 'string' ? error : error.message);
+    }
+  });
+
+  const renderForm = (
+    <Box gap={3} display="flex" flexDirection="column">
+      <Field.Text name="userId" label="userId address" InputLabelProps={{ shrink: true }} />
+
+      <Box gap={1.5} display="flex" flexDirection="column">
+        <Link
+          component={RouterLink}
+          href="#"
+          variant="body2"
+          color="inherit"
+          sx={{ alignSelf: 'flex-end' }}
+        >
+          Forgot password?
+        </Link>
+
+        <Field.Text
+          name="password"
+          label="Password"
+          placeholder="6+ characters"
+          type={password.value ? 'text' : 'password'}
+          InputLabelProps={{ shrink: true }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={password.onToggle} edge="end">
+                  <Iconify icon={password.value ? 'solar:eye-bold' : 'solar:eye-closed-bold'} />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Box>
+
+      <LoadingButton
+        fullWidth
+        color="inherit"
+        size="large"
+        type="submit"
+        variant="contained"
+        loading={isSubmitting}
+        loadingIndicator="Sign in..."
+      >
+        Sign in
+      </LoadingButton>
+    </Box>
+  );
+
+  return (
+    <>
+      <FormHead
+        title="Sign in to your account"
+        description={
+          <>
+            {`Don’t have an account? `}
+            <Link component={RouterLink} href={paths.auth.jwt.signUp} variant="subtitle2">
+              Get started
+            </Link>
+          </>
+        }
+        sx={{ textAlign: { xs: 'center', md: 'left' } }}
+      />
+
+      <Alert severity="info" sx={{ mb: 3 }}>
+        Use <strong>{defaultValues.userId}</strong>
+        {' with password '}
+        <strong>{defaultValues.password}</strong>
+      </Alert>
+
+      {!!errorMsg && (
+        <Alert severity="error" sx={{ mb: 3 }}>
+          {errorMsg}
+        </Alert>
+      )}
+
+      <Form methods={methods} onSubmit={onSubmit}>
+        {renderForm}
+      </Form>
+    </>
+  );
+}
