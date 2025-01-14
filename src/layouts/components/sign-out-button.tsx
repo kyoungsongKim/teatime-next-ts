49c6fc1746c6f36b82e541e6f3effe81c28c33d5
DESKTOP-842M40V\kskim <kskim@castis.com>
2025-01-14 19:08:02 +0900

    delete unused auth method. remove demo files

--- src/layouts/components/sign-out-button.tsx
+++ src/layouts/components/sign-out-button.tsx
@@ -14,17 +14,10 @@ import { toast } from 'src/components/snackbar';
 
 import { useAuthContext } from 'src/auth/hooks';
 import { signOut as jwtSignOut } from 'src/auth/context/jwt/action';
-import { signOut as amplifySignOut } from 'src/auth/context/amplify/action';
-import { signOut as supabaseSignOut } from 'src/auth/context/supabase/action';
-import { signOut as firebaseSignOut } from 'src/auth/context/firebase/action';
 
 // ----------------------------------------------------------------------
 
-const signOut =
-  (CONFIG.auth.method === 'supabase' && supabaseSignOut) ||
-  (CONFIG.auth.method === 'firebase' && firebaseSignOut) ||
-  (CONFIG.auth.method === 'amplify' && amplifySignOut) ||
-  jwtSignOut;
+const signOut = jwtSignOut;
 
 type Props = ButtonProps & {
   sx?: SxProps<Theme>;
@@ -69,7 +62,7 @@ export function SignOutButton({ onClose, ...other }: Props) {
       variant="soft"
       size="large"
       color="error"
-      onClick={CONFIG.auth.method === 'auth0' ? handleLogoutAuth0 : handleLogout}
+      onClick={handleLogout}
       {...other}
     >
       Logout
