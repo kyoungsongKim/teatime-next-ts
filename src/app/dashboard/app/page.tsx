b8e32c878db99a53ca7b071ce8174e80fe9d94fa
GayoungKim <gykim@castis.com>
2025-01-17 14:19:00 +0900

    [add] page.tsx - dashboard/page.tsx -> 대시보드용 컴포넌트 설정 - dashboard/app/page.tsx -> overview > app 컴포넌트 설정

--- src/app/dashboard/app/page.tsx
+++ src/app/dashboard/app/page.tsx
@@ -1,12 +1,11 @@
 import { CONFIG } from 'src/config-global';
 
-// import { OverviewAppView } from 'src/sections/overview/app/view';
-import { DashboardView } from '../../sections/dashboard/dashboard-view';
+import { OverviewAppView } from '../../../sections/overview/app/view';
 
 // ----------------------------------------------------------------------
 
 export const metadata = { title: `Dashboard - ${CONFIG.appName}` };
 
 export default function Page() {
-  return <DashboardView />;
+  return <OverviewAppView />;
 }
