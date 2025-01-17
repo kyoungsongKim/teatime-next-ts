815eb2fcf1c332180c348cbd0dbe7128b585fd64
GayoungKim <gykim@castis.com>
2025-01-17 14:10:48 +0900

    [change] config-nav-dashboard.tsx - dashboard 메뉴 추가 - app 메뉴 경로 수정

--- src/layouts/config-nav-dashboard.tsx
+++ src/layouts/config-nav-dashboard.tsx
@@ -45,12 +45,19 @@ const ICONS = {
 
 export const navData = [
   /**
+   *
+   */
+  {
+    subheader: '',
+    items: [{ title: 'Dashboard', path: paths.dashboard.root, icon: ICONS.dashboard }],
+  },
+  /**
    * Overview
    */
   {
     subheader: 'Overview',
     items: [
-      { title: 'App', path: paths.dashboard.root, icon: ICONS.dashboard },
+      { title: 'App', path: paths.dashboard.app, icon: ICONS.dashboard },
       { title: 'Ecommerce', path: paths.dashboard.general.ecommerce, icon: ICONS.ecommerce },
       { title: 'Analytics', path: paths.dashboard.general.analytics, icon: ICONS.analytics },
       { title: 'Banking', path: paths.dashboard.general.banking, icon: ICONS.banking },
