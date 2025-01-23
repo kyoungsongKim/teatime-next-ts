01f3b625c5e5348c935e7db85abc5cb539128b27
GayoungKim <gykim@castis.com>
2025-01-23 18:05:41 +0900

    [add] sections/point/dialog/point-create-dialog.tsx - 포인트 생성 다이얼로그 추가

---
+++ src/sections/point/dialog/point-create-dialog.tsx
@@ -0,0 +1,191 @@
+import type { CUserItem } from 'src/types/user';
+import type { CreatePointItem } from 'src/types/point';
+
+import * as zod from 'zod';
+import { toast } from 'sonner';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import Stack from '@mui/material/Stack';
+import Dialog from '@mui/material/Dialog';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import LoadingButton from '@mui/lab/LoadingButton';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import InputAdornment from '@mui/material/InputAdornment';
+
+import { useBoolean } from 'src/hooks/use-boolean';
+
+import { makePoint } from 'src/actions/point-ssr';
+
+import { Iconify } from 'src/components/iconify';
+import { Form, Field } from 'src/components/hook-form';
+import { ConfirmDialog } from 'src/components/custom-dialog';
+
+export const CreatePointSchema = zod.object({
+  sender: zod
+    .string()
+    .min(1, { message: '기부코드 전달자에 대한 정보가 만료되었습니다. 다시 로그인해주세요.' }),
+  receiver: zod.object({
+    id: zod.string().min(1, { message: '기부코드를 받을 사람을 선택해주세요.' }),
+  }),
+  memo: zod.string().min(1, { message: '메모를 입력해주세요.' }),
+  point: zod.number().min(0, { message: '포인트를 입력해주세요.' }).or(zod.literal(0)),
+});
+
+type Props = {
+  id: string;
+  open: boolean;
+  userList: CUserItem[];
+  onClose: () => void;
+};
+
+export function PointCreateDialog({ id, open, userList, onClose }: Props) {
+  const confirm = useBoolean();
+  const [publishCode, setPublishCode] = useState('');
+
+  const defaultPointValue: CreatePointItem = {
+    sender: id,
+    receiver: null,
+    memo: '',
+    point: 0,
+  };
+  const methods = useForm<CreatePointItem>({
+    mode: 'all',
+    resolver: zodResolver(CreatePointSchema),
+    defaultValues: defaultPointValue,
+  });
+  const {
+    reset,
+    watch,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
+
+  const onCreate = handleSubmit(async (data) => {
+    const receiver = data.receiver ?? { id: '' };
+    try {
+      await makePoint(data.sender, receiver.id, data.point, data.memo).then((r) => {
+        console.log('make point', r);
+        if (r.status !== 200) {
+          toast.error(r.data);
+        } else {
+          setPublishCode(r.data);
+          confirm.onTrue();
+          reset();
+          onClose();
+        }
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  return (
+    <>
+      <Dialog fullWidth maxWidth="md" open={open} onClose={onClose}>
+        <DialogTitle>
+          <Stack spacing={1} direction="row" alignItems="center" justifyContent="space-between">
+            Create Point
+            <IconButton onClick={onClose}>
+              <Iconify icon="eva:close-fill" />
+            </IconButton>
+          </Stack>
+        </DialogTitle>
+        <Form methods={methods} onSubmit={onCreate}>
+          <DialogContent>
+            <Stack spacing={2}>
+              <Stack spacing={1}>
+                <Typography variant="subtitle2">Choice ID</Typography>
+                <Field.Autocomplete
+                  options={userList || []}
+                  name="receiver"
+                  size="small"
+                  value={watch('receiver')}
+                  isOptionEqualToValue={(option, value) => option.id === value.id}
+                  getOptionLabel={(option) =>
+                    option && option.id && option.realName
+                      ? `${option.id} (${option.realName})`
+                      : ''
+                  }
+                  renderOption={(props, option) => (
+                    <li {...props} key={option.id}>
+                      {option.id} ({option.realName})
+                    </li>
+                  )}
+                />
+              </Stack>
+              <Stack spacing={1}>
+                <Typography variant="subtitle2">Memo</Typography>
+                <Field.Text name="memo" size="small" sx={{ '.Mui-error': { mx: 0 } }} />
+              </Stack>
+              <Stack spacing={1}>
+                <Typography variant="subtitle2">Point</Typography>
+                <Field.Text
+                  type="number"
+                  name="point"
+                  size="small"
+                  placeholder="0"
+                  sx={{ '.Mui-error': { mx: 0 } }}
+                />
+              </Stack>
+            </Stack>
+          </DialogContent>
+          <DialogActions sx={{ justifyContent: 'center' }}>
+            <Button variant="soft" onClick={() => reset()}>
+              Reset
+            </Button>
+            <LoadingButton
+              type="submit"
+              variant="soft"
+              color="primary"
+              loading={isSubmitting}
+              disabled={isSubmitting}
+            >
+              Create
+            </LoadingButton>
+          </DialogActions>
+        </Form>
+      </Dialog>
+
+      <ConfirmDialog
+        open={confirm.value}
+        onClose={confirm.onFalse}
+        title="코드 발행"
+        content={
+          <>
+            <Typography variant="body2">포인트 기부용 코드가 발급되었습니다.</Typography>
+            <TextField
+              fullWidth
+              size="small"
+              variant="outlined"
+              value={publishCode}
+              InputProps={{
+                readOnly: true,
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={() => {
+                        navigator.clipboard.writeText(publishCode);
+                        toast.success('copied!');
+                      }}
+                      edge="end"
+                    >
+                      <Iconify icon="eva:copy-fill" />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </>
+        }
+        action={<></>}
+      />
+    </>
+  );
+}
