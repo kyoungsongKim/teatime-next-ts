94006173b8cc979ad6af9b317ca2468e8d77a355
GayoungKim <gykim@castis.com>
2025-01-24 16:29:05 +0900

    [add] utils/user-info.ts - token으로 user 정보 추출하는 공통 함수 추가

---
+++ src/utils/user-info.ts
@@ -0,0 +1,10 @@
+import { jwtDecode } from '../auth/context/jwt';
+import { UserType } from '../auth/types';
+
+export function getUserInfo(user: UserType) {
+  // 사용자 정보 불러오기
+  if (!user) {
+    return { id: '', auth: '' };
+  }
+  return jwtDecode(user?.accessToken);
+}
