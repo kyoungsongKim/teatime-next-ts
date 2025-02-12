46ad3b3264fa591bdb08c7db2fd5c1ff97ad171a
GayoungKim <gykim@castis.com>
2025-02-12 17:31:00 +0900

    [change] vacation-form-dialog.tsx - 시작일을 기준으로 휴가 일수에 맞춰 계산하도록 수정

--- src/sections/vacation/dialog/vacation-form-dialog.tsx
+++ src/sections/vacation/dialog/vacation-form-dialog.tsx
@@ -5,8 +5,8 @@ import dayjs from 'dayjs';
 import * as zod from 'zod';
 import { toast } from 'sonner';
 import { useForm } from 'react-hook-form';
+import { useMemo, useState, useEffect, useCallback } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useMemo, useEffect, useCallback } from 'react';
 
 import { Button } from '@mui/material';
 import Stack from '@mui/material/Stack';
@@ -15,18 +15,21 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import LoadingButton from '@mui/lab/LoadingButton';
 import DialogTitle from '@mui/material/DialogTitle';
+import ToggleButton from '@mui/material/ToggleButton';
 import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
+import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider, renderDigitalClockTimeView } from '@mui/x-date-pickers';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
-import { calcDateDiff, makeDateString } from 'src/utils/format-date';
+import { getNextBusinessDate, makeDateString } from 'src/utils/format-date';
 
 import { saveVacation, updateVacation } from 'src/actions/vacation';
 
 import { Iconify } from 'src/components/iconify';
 import { Scrollbar } from 'src/components/scrollbar';
 import { Form, Field } from 'src/components/hook-form';
+import { fNumber } from 'src/utils/format-number';
 
 const VacationHistorySchema = (auth: string) =>
   zod
@@ -34,6 +37,7 @@ const VacationHistorySchema = (auth: string) =>
       eventStartDate: zod.union([zod.string(), zod.number()]),
       eventEndDate: zod.union([zod.string(), zod.number()]),
       type: zod.string(),
+      amount: zod.number().min(0, { message: '휴가 일수는 0일 이상이어야 합니다.' }),
       reason: zod.string().min(1, { message: '사유를 입력해주세요.' }),
       adminMemo: zod.string().refine((value) => auth !== 'ADMIN' || value.length > 0, {
         message: '관리자 메모를 입력해주세요.',
@@ -71,6 +75,10 @@ export function VacationFormDialog({
   onUpdate,
 }: Props) {
   const vacationType = ['연차', '공가', '경조', '출산', '특별', '보상', '기타'];
+
+  const [selectedOption, setSelectedOption] = useState<string>('연차');
+  const [selectedTime, setSelectedTime] = useState<string>('09:00');
+
   const today = useMemo(() => new Date(), []);
   const defaultStartDate = useMemo(
     () => new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1, 9, 0, 0),
@@ -111,30 +119,11 @@ export function VacationFormDialog({
     handleSubmit,
     formState: { isSubmitting },
   } = methods;
-  const values = watch();
-
-  const vacationAmount = useCallback((eStartDate: string, eEndDate: string) => {
-    const startDate = new Date(eStartDate);
-    startDate.setHours(0, 0, 0, 0);
-    const endDate = new Date(eEndDate);
-    endDate.setHours(0, 0, 0, 0);
-
-    if (startDate.getTime() > endDate.getTime()) {
-      return -1;
-    }
-
-    let amount = 0;
-    amount += calcDateDiff(endDate, startDate) + 1;
-    if ([13, 14, 15, 16].includes(new Date(eStartDate).getHours())) amount -= 0.5;
-    if ([14, 15, 16].includes(new Date(eEndDate).getHours())) amount -= 0.5;
-    return amount;
-  }, []);
 
   const onSubmit = handleSubmit(async (data) => {
     const params: VacationHistoryItem = {
       ...data,
       userId: user,
-      amount: vacationAmount(data.eventStartDate, data.eventEndDate),
       eventStartDate: makeDateString(new Date(data.eventStartDate), 7),
       eventEndDate: makeDateString(new Date(data.eventEndDate), 7),
     };
@@ -147,6 +136,8 @@ export function VacationFormDialog({
           if (r.status === 200) {
             toast.success('수정되었습니다.');
             reset(defaultValues);
+            setSelectedTime('09:00');
+            setSelectedOption('연차');
             onUpdate();
             onClose();
           } else {
@@ -163,6 +154,8 @@ export function VacationFormDialog({
           if (r.status === 200) {
             toast.success('저장되었습니다.');
             reset(defaultValues);
+            setSelectedTime('09:00');
+            setSelectedOption('연차');
             onUpdate();
             onClose();
           } else {
@@ -175,57 +168,112 @@ export function VacationFormDialog({
     }
   });
 
-  const hours = [
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
-  ];
+  const countAmount = useCallback(
+    (value: number, init = false) => {
+      methods.setValue('amount', init ? value : watch('amount') + value);
+      updateDates();
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [methods, watch]
+  );
 
-  const filterHour = (...targetHours: number[]) =>
-    hours.filter((hour) => !targetHours.includes(hour));
+  const timeSelectOption = useMemo(() => {
+    let result = [];
+    if (selectedOption === '연차') {
+      setSelectedTime('09:00');
+      return [];
+    }
+    if (selectedOption.includes('오전')) {
+      result = ['09:00', '10:00'];
+    } else if (selectedOption.includes('반반차')) {
+      result = ['16:00', '17:00'];
+    } else {
+      result = ['14:00', '15:00'];
+    }
 
-  const eventEndTimeDisableChecker = () => {
-    if (!values.eventEndDate) return hours;
-    const clonedEventEndDate = new Date(values.eventEndDate);
-    clonedEventEndDate.setHours(new Date(defaultEndDate).getHours() || 0, 0, 0, 0);
-    const amount = vacationAmount(values.eventStartDate, makeDateString(clonedEventEndDate, 6));
-    if (amount <= 1) {
-      let startTime = new Date(values.eventStartDate).getHours();
-      if (startTime < 12) {
-        startTime += 1;
-        return filterHour(startTime + 4, startTime + 8);
-      }
-      return filterHour(startTime + 4);
+    if (!result.includes(selectedTime)) {
+      setSelectedTime(result[0]);
     }
-    if (amount > left) {
-      return filterHour(14, 15, 16);
+
+    return result;
+  }, [selectedOption, selectedTime]);
+
+  const handleSelection = (event: React.MouseEvent<HTMLElement>, newValue: string) => {
+    // 연차 선택 시 개수 조절 가능하도록 기본값 1 설정
+    const targetValue = newValue === null ? selectedOption : newValue;
+    if (selectedOption !== targetValue && targetValue === '연차') {
+      countAmount(1, true);
+    } else if (selectedOption === targetValue && targetValue === '연차') {
+      countAmount(1);
     }
-    return filterHour(14, 15, 16, 18, 19, 20);
+    setSelectedOption(targetValue);
   };
+
+  const getHourOffset = (hour: number, option: string) => {
+    if (option.includes('반반차')) {
+      return 2; // 오전 반반차
+    }
+
+    if (option.includes('반차')) {
+      if (hour === 9 || hour === 10) return 5; // 오전 반차 (점심 포함)
+      if (hour === 14 || hour === 15) return 4; // 오후 반차
+    }
+
+    return 0;
+  };
+
+  const updateDates = useCallback(() => {
+    const startDate = dayjs(methods.getValues('eventStartDate'));
+    const amount = methods.getValues('amount');
+    const hour = parseInt(selectedTime.split(':')[0], 10);
+    const updatedStart = startDate.hour(hour);
+    const hourOffset = getHourOffset(hour, selectedOption);
+
+    methods.setValue('eventStartDate', updatedStart.format('YYYY-MM-DD HH:mm'));
+
+    if (selectedOption.includes('연차')) {
+      methods.setValue('eventEndDate', getNextBusinessDate(startDate.hour(18), amount));
+    } else {
+      methods.setValue(
+        'eventEndDate',
+        updatedStart.add(hourOffset, 'hour').format('YYYY-MM-DD HH:mm')
+      );
+    }
+  }, [methods, selectedTime, selectedOption]);
+
+  const setInitData = useCallback((start: string, end: string) => {
+    const startTime = dayjs(start).hour();
+    const endTime = dayjs(end).hour();
+
+    if (startTime === 9 && endTime === 18) {
+      setSelectedOption('연차');
+    } else if ((startTime === 9 && endTime === 11) || (startTime === 10 && endTime === 12)) {
+      setSelectedOption('반반차(오전)');
+    } else if ((startTime === 9 && endTime === 14) || (startTime === 10 && endTime === 15)) {
+      setSelectedOption('반차(오전)');
+    } else if ((startTime === 14 && endTime === 18) || (startTime === 15 && endTime === 19)) {
+      setSelectedOption('반차(오후)');
+    } else {
+      setSelectedOption('반반차(오후)');
+    }
+    setSelectedTime(`${String(startTime).padStart(2, '0')}:00`);
+  }, []);
+
+  useEffect(() => {
+    updateDates();
+  }, [updateDates]);
 
   useEffect(() => {
     if (item) {
       reset(item);
+      if (item.type === '경조휴가') methods.setValue('type', '경조');
+      setInitData(item.eventStartDate, item.eventEndDate);
     } else {
+      setSelectedOption('연차');
+      setSelectedTime('09:00');
       reset(defaultValues);
     }
-  }, [defaultValues, item, reset]);
-
-  useEffect(() => {
-    const watchEventStartDate = methods.getValues('eventStartDate');
-    const watchEventEndDate = methods.getValues('eventEndDate');
-
-    const amount = vacationAmount(watchEventStartDate, watchEventEndDate);
-    if (amount === -1) {
-      methods.setValue('eventEndDate', dayjs(watchEventStartDate).hour(18).format());
-    }
-    if (amount > 0 && amount < 1) {
-      let addDays = 4;
-      if (new Date(watchEventStartDate).getHours() < 12) {
-        addDays = 5;
-      }
-      methods.setValue('eventEndDate', dayjs(watchEventStartDate).add(addDays, 'hour').format());
-    }
-    // eslint-disable-next-line
-  }, [watch('eventStartDate'), watch('eventEndDate')]);
+  }, [defaultValues, item, methods, reset, setInitData]);
 
   return (
     <Dialog fullWidth maxWidth="sm" open={open} onClose={onClose}>
@@ -242,78 +290,99 @@ export function VacationFormDialog({
           <DialogContent>
             <Stack spacing={1}>
               {/* 휴가 기간 */}
-              <Typography variant="subtitle2">기간</Typography>
+              <Stack spacing={1}>
+                <Typography variant="subtitle2">휴가 기간</Typography>
+                <Typography variant="caption">
+                  시작일 설정 후, 휴가 사용일에 맞춰 종료일(주말 제외)이 자동으로 계산됩니다.
+                </Typography>
+              </Stack>
               <Stack spacing={1} direction={{ xs: 'column', sm: 'column', md: 'row' }}>
                 <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ko">
-                  <Field.MobileDateTimePicker
+                  <Field.DatePicker
                     name="eventStartDate"
-                    ampm={false}
                     format="YYYY-MM-DD HH:mm"
-                    viewRenderers={{
-                      hours: renderDigitalClockTimeView,
-                      minutes: undefined,
-                    }} // 분 뷰 비활성화
-                    disablePast={auth !== 'ADMIN'} // 관리자 제외 과거 연차는 작성할 수 없음
+                    disablePast={auth !== 'ADMIN'}
                     minDate={auth !== 'ADMIN' ? dayjs().add(1, 'day') : undefined} // 관리자만 당일 휴가 신청을 작성할 수 있음
-                    shouldDisableTime={(value, clockType) => {
-                      if (clockType === 'hours') {
-                        // 허용되지 않은 시간을 비활성화
-                        return ![9, 10, 11, 13, 14, 15, 16].includes(value.hour());
-                      }
-                      return false;
-                    }}
-                    minutesStep={60} // 60분 단위
-                    slotProps={{
-                      toolbar: {
-                        toolbarFormat: 'MM월 DD일',
-                        toolbarPlaceholder: '날짜 선택',
-                      },
-                      dialog: {
-                        sx: {
-                          '& .MuiDigitalClock-root': {
-                            minWidth: { xs: 320 },
-                          },
-                        },
-                      },
-                    }}
                     disabled={auth !== 'ADMIN' && !!item}
                   />
-                  <Field.MobileDateTimePicker
-                    name="eventEndDate"
-                    ampm={false}
-                    format="YYYY-MM-DD HH:mm"
-                    viewRenderers={{ hours: renderDigitalClockTimeView, minutes: undefined }}
-                    disablePast={auth !== 'ADMIN'} // 관리자 제외 과거 연차는 작성할 수 없음
-                    minDate={dayjs(values.eventStartDate)} // 관리자만 당일 휴가 신청을 작성할 수 있음
-                    maxDate={dayjs(values.eventStartDate).add(left, 'day')}
-                    minutesStep={60}
-                    slotProps={{
-                      toolbar: {
-                        toolbarFormat: 'MM월 DD일',
-                        toolbarPlaceholder: '날짜 선택',
-                      },
-                      dialog: {
-                        sx: {
-                          '& .MuiDigitalClock-root': {
-                            minWidth: { xs: 320 },
-                          },
-                        },
-                      },
-                    }}
-                    disabled={auth !== 'ADMIN' && !!item}
-                    shouldDisableTime={(value, clockType) => {
-                      if (clockType === 'hours') {
-                        // 허용되지 않은 시간을 비활성화
-                        return eventEndTimeDisableChecker().includes(value.hour());
-                      }
-                      return false;
-                    }}
-                  />
+
+                  <Field.DatePicker name="eventEndDate" format="YYYY-MM-DD HH:mm" disabled />
                 </LocalizationProvider>
               </Stack>
-              <Typography textAlign="center" variant="body2">
-                (총 {vacationAmount(watch('eventStartDate'), watch('eventEndDate'))}일)
-              </Typography>
+              <Stack spacing={1} direction="row" alignItems="center">
+                <Typography variant="body1">총 {watch('amount')}일</Typography>
+                <IconButton
+                  size="small"
+                  onClick={() => {
+                    setSelectedOption('연차');
+                    methods.setValue('amount', 1);
+                    updateDates();
+                  }}
+                  disabled={auth !== 'ADMIN' && !!item}
+                >
+                  <Iconify icon="eva:refresh-outline" />
+                </IconButton>
+              </Stack>
+              <Stack spacing={3}>
+                {/* 반차 / 반반차 / 연차 선택 */}
+                <ToggleButtonGroup
+                  value={selectedOption}
+                  exclusive
+                  onChange={handleSelection}
+                  aria-label="vacation selection"
+                  color="primary"
+                  sx={{
+                    flexWrap: 'wrap',
+                    '.MuiToggleButton-root': {
+                      flex: {
+                        xs: '1 0 calc(25% - 8px)',
+                        sm: '1 0 calc(25% - 8px)',
+                        md: '1 1 auto',
+                      },
+                      textWrap: 'nowrap',
+                    },
+                  }}
+                  disabled={auth !== 'ADMIN' && !!item}
+                >
+                  <ToggleButton value="반반차(오전)" onClick={() => countAmount(0.25, true)}>
+                    반반차 (오전)
+                  </ToggleButton>
+                  <ToggleButton value="반차(오전)" onClick={() => countAmount(0.5, true)}>
+                    반차 (오전)
+                  </ToggleButton>
+                  <ToggleButton value="연차">
+                    연차({selectedOption === '연차' ? watch('amount') : 1})
+                  </ToggleButton>
+                  <ToggleButton value="반차(오후)" onClick={() => countAmount(0.5, true)}>
+                    반차 (오후)
+                  </ToggleButton>
+                  <ToggleButton value="반반차(오후)" onClick={() => countAmount(0.25, true)}>
+                    반반차 (오후)
+                  </ToggleButton>
+                </ToggleButtonGroup>
+              </Stack>
+
+              {/* 반반차, 반차일 경우엔 시간 선택지 추가 */}
+              {selectedOption.includes('반차') && (
+                <ToggleButtonGroup
+                  color="primary"
+                  exclusive
+                  fullWidth
+                  value={selectedTime}
+                  disabled={auth !== 'ADMIN' && !!item}
+                  onChange={(e, newValue) => {
+                    if (newValue !== null) {
+                      setSelectedTime(newValue);
+                    }
+                  }}
+                >
+                  {timeSelectOption.map((time) => (
+                    <ToggleButton key={time} value={time}>
+                      {time}
+                    </ToggleButton>
+                  ))}
+                </ToggleButtonGroup>
+              )}
 
               {/* 휴가 구분(연차, 공가, 경조, 출산, 특별, 보상, 기타) */}
               <Typography variant="subtitle2">구분</Typography>
@@ -348,7 +417,13 @@ export function VacationFormDialog({
           </DialogContent>
         </Scrollbar>
         <DialogActions sx={{ justifyContent: 'center' }}>
-          <Button onClick={() => reset(defaultValues)} disabled={auth !== 'ADMIN' && !!item}>
+          <Button
+            onClick={() => {
+              setSelectedOption('연차');
+              reset(defaultValues);
+            }}
+            disabled={auth !== 'ADMIN' && !!item}
+          >
             초기화
           </Button>
           <LoadingButton
