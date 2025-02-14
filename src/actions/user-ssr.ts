3c349439d8d26f9438293c22ef40f8680bebb1e1
GayoungKim <gykim@castis.com>
2025-02-14 11:13:23 +0900

    [fix] user-ssr.ts - getUserList -> /api/users 이용하도록 수정 -  deleteUserInfo -> /api/user 이용하도록 수정

--- src/actions/user-ssr.ts
+++ src/actions/user-ssr.ts
@@ -1,13 +1,13 @@
 import axios, { endpoints } from '../utils/axios';
 
 export async function getUserList(except?: string) {
-  const URL = `${endpoints.users.root}${except ? `?except=${except}` : ''}`;
+  const URL = `${endpoints.user.list}${except ? `?except=${except}` : ''}`;
   const res = await axios.get(URL);
   return res;
 }
 
 export async function deleteUserInfo(userId: string) {
-  const URL = `${endpoints.users.root}/${userId}`;
+  const URL = `${endpoints.user.root}/${userId}`;
   const res = await axios.delete(URL);
 
   return res;
