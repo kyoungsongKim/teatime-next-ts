76acd1184cba08b98eb95da7a4c7f0043ec7913f
GayoungKim <gykim@castis.com>
2025-02-19 18:21:28 +0900

    [add] (home)/calendar/page.tsx - 캘린더 페이지 연결

--- src/app/(home)/calendar/page.tsx
+++ src/app/(home)/calendar/page.tsx
@@ -2,9 +2,7 @@ import { CONFIG } from 'src/config-global';
 
 import { CalendarView } from 'src/sections/calendar/calendar-view';
 
-// ----------------------------------------------------------------------
-
-export const metadata = { title: `Calendar | Dashboard - ${CONFIG.appName}` };
+export const metadata = { title: `Calendar - ${CONFIG.appName}` };
 
 export default function Page() {
   return <CalendarView />;
