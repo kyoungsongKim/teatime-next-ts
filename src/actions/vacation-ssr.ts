46d42d56202a2a0c8400659b430bb1a87272f313
GayoungKim <gykim@castis.com>
2025-02-19 18:13:22 +0900

    [add] actions/vacation-ssr.ts - 휴가 list API 추가

--- src/actions/vacation-ssr.ts
+++ src/actions/vacation-ssr.ts
@@ -1,4 +1,9 @@
-import axios, { endpoints } from 'src/utils/axios';
+import type { VacationHistoryItem } from 'src/types/vacation';
+
+import useSWR from 'swr';
+import { useMemo } from 'react';
+
+import axios, { fetcher, endpoints } from 'src/utils/axios';
 
 export async function getVacationList(id: string, workedYear?: number) {
   const URL = `${endpoints.vacation.root}?userId=${id}${workedYear ? `&workedYear=${workedYear}` : ''}`;
@@ -11,4 +16,54 @@ export async function getVacationAll() {
   const res = await axios.get(endpoints.vacation.all);
 
   return res;
+}
+
+const enableServer = true;
+const VACATION_ROOT_ENDPOINT = endpoints.vacation.root;
+const VACATION_LIST_ENDPOINT = endpoints.vacation.list;
+
+const swrOptions = {
+  revalidateIfStale: enableServer,
+  revalidateOnFocus: enableServer,
+  revalidateOnReconnect: enableServer,
+};
+
+export function useGetVacation(id: string, userName: string, date: Date) {
+  const listKey =
+    !userName || !date || !id
+      ? null
+      : [
+          VACATION_LIST_ENDPOINT,
+          {
+            params: {
+              userId: userName,
+              year: date.getFullYear(),
+            },
+          },
+        ];
+  const key = !userName || !date || !id ? null : [`${VACATION_ROOT_ENDPOINT}/${id}`];
+
+  const {
+    data: listData,
+    isLoading: listIsLoading,
+    error: listError,
+    isValidating: listIsValidating,
+  } = useSWR<VacationHistoryItem[]>(listKey, fetcher, swrOptions);
+
+  const { data, isLoading, error, isValidating } = useSWR<VacationHistoryItem>(
+    key,
+    fetcher,
+    swrOptions
+  );
+
+  return useMemo(
+    () => ({
+      histories: listData,
+      item: data,
+      isLoading: listIsLoading || isLoading,
+      error: listError || error,
+      isValidating: listIsValidating || isValidating,
+    }),
+    [data, error, isLoading, isValidating, listData, listError, listIsLoading, listIsValidating]
+  );
 }
