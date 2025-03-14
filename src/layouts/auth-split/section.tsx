44a8a689df7ba8994261db4f02978e1c96d4183e
mcalba <kskim0428@castis.com>
2025-03-14 22:07:31 +0900

    [fix] 오픈을 위한 메뉴 주석 처리

--- src/layouts/auth-split/section.tsx
+++ src/layouts/auth-split/section.tsx
@@ -34,7 +34,7 @@ export function Section({
   methods,
   title = 'Manage the job',
   imgUrl = `${CONFIG.assetsDir}/assets/illustrations/illustration-dashboard.webp`,
-  subtitle = 'More effectively with optimized workflows.',
+  subtitle = 'Agency Division Service Site',
   ...other
 }: SectionProps) {
   const theme = useTheme();
